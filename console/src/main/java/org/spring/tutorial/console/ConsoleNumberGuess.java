ae59ddfd3acf26ab1444b849a660b307502a562c
Kumar, Abhishek <abhishek.kumar3@tyson.com>
2021-09-04 07:52:02 +0530

    alternate way of implempting event listener

--- console/src/main/java/org/spring/tutorial/console/ConsoleNumberGuess.java
+++ console/src/main/java/org/spring/tutorial/console/ConsoleNumberGuess.java
@@ -2,18 +2,17 @@ package org.spring.tutorial.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+public class ConsoleNumberGuess {
 
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-
+    @EventListener(ContextRefreshedEvent.class)
+    public void start() {
         log.info("start() --> Container ready for use.");
 
     }
