c9464ddae13308b8ee8883fd02f929f400234317
Kumar, Abhishek <abhishek.kumar3@tyson.com>
2021-09-04 10:46:20 +0530

    add properties file to app

--- core/src/main/java/org/spring/tutorial/GameImpl.java
+++ core/src/main/java/org/spring/tutorial/GameImpl.java
@@ -33,8 +33,8 @@ public class GameImpl implements Game {
     @Override
     public void reset() {
 
-        smallest = 0;
-        guess = 0;
+        smallest = numberGenerator.getMinNumber();
+        guess = numberGenerator.getMinNumber();
         remainingGuess = guessCount;
         biggest = numberGenerator.getMaxNumber();
         number = numberGenerator.next();
--- core/src/main/java/org/spring/tutorial/config/GameConfig.java
+++ core/src/main/java/org/spring/tutorial/config/GameConfig.java
@@ -2,15 +2,25 @@ package org.spring.tutorial.config;
 
 import org.spring.tutorial.GuessCount;
 import org.spring.tutorial.MaxNumber;
+import org.spring.tutorial.MinNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
 
-    private int maxNumber = 20;
-    private int guessCount = 8;
+    @Value("${game.maxNumber}")
+    private int maxNumber;
+    @Value("${game.guessCount}")
+    private int guessCount;
+
+    @Value("${game.minNumber:10}")
+    private int minNumber;
+
 
     @Bean
     @MaxNumber
@@ -22,5 +32,11 @@ public class GameConfig {
     @GuessCount
     public int getGuessCount() {
         return guessCount;
+    }
+
+    @Bean
+    @MinNumber
+    public int getMinNumber(){
+        return minNumber;
     }
 }
