16da5e7d28f49c50638d19d6933a8289454f27bc
Kumar, Abhishek <abhishek.kumar3@tyson.com>
2021-09-07 21:27:07 +0530

    add spring boot web module

---
+++ web/src/main/java/org/spring/tutorial/WebMain.java
@@ -0,0 +1,13 @@
+package org.spring.tutorial;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMain.class,args);
+    }
+}
